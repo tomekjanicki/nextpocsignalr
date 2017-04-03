66840dfc126464567509a2b89a6b2b98fb73ff00
Tomasz Janicki <tomaszjanicki@ciber.com>
2017-04-03 11:31:33 +0200

    xxx

--- WebRealTime/WhiteBoardHub.cs
+++ WebRealTime/WhiteBoardHub.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 
 namespace WebRealTime
@@ -7,20 +8,33 @@ namespace WebRealTime
     {
         public void SendNewAddedItem(int item, int page)
         {
+            CheckSecurity();
             var pageId = page.ToString();
             Clients.Group(pageId).broadcastMessage(item);
         }
 
         public async Task JoinPage(int page)
         {
+            CheckSecurity();
             var pageId = page.ToString();
             await Groups.Add(Context.ConnectionId, pageId).ConfigureAwait(false);
         }
 
         public Task LeavePage(int page)
         {
+            CheckSecurity();
             var pageId = page.ToString();
             return Groups.Remove(Context.ConnectionId, pageId);
+        }
+
+        private void CheckSecurity()
+        {
+            if (Context.RequestCookies.ContainsKey("sessionId"))
+            {
+                return;
+            }
+
+            throw new InvalidOperationException();
         }
     }
 }
