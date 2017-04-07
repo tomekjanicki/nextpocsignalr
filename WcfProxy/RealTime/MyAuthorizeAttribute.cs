1f1d6d8490aeeee6c408fced0d7917a9c2fe0195
Tomasz Janicki <tomaszjanicki@ciber.com>
2017-04-07 12:37:04 +0200

    implement security in a better way

---
+++ WcfProxy/RealTime/MyAuthorizeAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Shared;
+using WcfProxy.Service;
+
+namespace WcfProxy.RealTime
+{
+    public sealed class MyAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext hubIncomingInvokerContext, bool appliesToMethod)
+        {
+            return CheckSecurity(hubIncomingInvokerContext.Hub.Context);
+        }
+
+        public override bool AuthorizeHubConnection(HubDescriptor hubDescriptor, IRequest request)
+        {
+            return true;
+        }
+
+        private static bool CheckSecurity(HubCallerContext hubCallerContext)
+        {
+            try
+            {
+                using (var client = new ServiceClient())
+                {
+                    client.ShouldSendNotification(GetContextData(hubCallerContext.RequestCookies));
+                    return true;
+                }
+            }
+            catch (FaultException)
+            {
+                return false;
+            }
+        }
+
+        private static WebContextData GetContextData(IDictionary<string, Cookie> cookies)
+        {
+            return new WebContextData
+            {
+                CookiesIn = cookies.ToDictionary(pair => pair.Value.Name, pair => pair.Value.Value.ToString())
+            };
+        }
+
+    }
+}
--- WcfProxy/RealTime/WhiteBoardHubV2.cs
+++ WcfProxy/RealTime/WhiteBoardHubV2.cs
@@ -1,64 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using Shared;
-using WcfProxy.Service;
 
 namespace WcfProxy.RealTime
 {
+    [MyAuthorize]
     public sealed class WhiteBoardHubV2 : Hub<IClinetV2>
     {
         public void SquareDelete(Guid id, int page)
         {
-            CheckSecurity();
             var pageId = page.ToString();
             Clients.OthersInGroup(pageId).SquareDeleted(id);
         }
 
         public void SquareMove(Square square, int page)
         {
-            CheckSecurity();
             var pageId = page.ToString();
             Clients.OthersInGroup(pageId).SquareMoved(square);
         }
 
         public void SquareAdd(Square square, int page)
         {
-            CheckSecurity();
             var pageId = page.ToString();
             Clients.OthersInGroup(pageId).SquareAdded(square);
         }
 
         public async Task JoinPage(int page)
         {
-            CheckSecurity();
             var pageId = page.ToString();
             await Groups.Add(Context.ConnectionId, pageId).ConfigureAwait(false);
         }
 
         public Task LeavePage(int page)
         {
-            CheckSecurity();
             var pageId = page.ToString();
             return Groups.Remove(Context.ConnectionId, pageId);
-        }
-
-        private void CheckSecurity()
-        {
-            using (var client = new ServiceClient())
-            {
-                client.ShouldSendNotification(GetContextData(Context.RequestCookies));
-            }
-        }
-
-        private static WebContextData GetContextData(IDictionary<string, Cookie> cookies)
-        {
-            return new WebContextData
-            {
-                CookiesIn = cookies.ToDictionary(pair => pair.Value.Name, pair => pair.Value.Value.ToString())
-            };
         }
     }
 }
